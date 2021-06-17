55d487b5205376e8f2bd95efdd14e124540fb1c2
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-06-17 12:14:42 +0800

    Add files via upload

--- Basic_Buliding_Block_GPU/GPU_Layers.py
+++ Basic_Buliding_Block_GPU/GPU_Layers.py
@@ -4,7 +4,7 @@ from numba import cuda,float64,int64
 import time
 import Layers
 
-@cuda.jit
+@cuda.jit("float64[:,:,:,:],float64[:,:,:,:],float64[:,:,:,:],float64[:,:,:,:],int64,int64,int64,int64")
 def conv_step_forward4D(W,img,b,Z,stride,xlim,ylim,zlim):
     
     """
@@ -172,6 +172,52 @@ if __name__ == "__main__":
 
     #4D
     #GPU
+    W = np.random.randn(3,3,3,16).astype(np.float64)
+    b = np.random.randn(1,1,1,16).astype(np.float64)
+    img = np.random.randn(2,1080,1920,3)
+
+    fH,fW,n_C_prev,n_C = W.shape
+    m,n_H_prev,n_W_prev,n_C_prev = img.shape[1],img.shape[2]
+    stride = 2
+
+    n_H = int((n_H_prev-fH)/stride)+1
+    n_W = int((n_W_prev-fW)/stride)+1
+
+    Z = np.zeros(m,n_H,n_W,n_C)
+
+    gpu_time = time.time()
+
+    threadsperblock = (8,8,2)
+
+    blockspergrid_H = int(math.ceil(n_H/threadsperblock[0]))
+    blockspergrid_W = int(math.ceil(n_W/threadsperblock[1]))
+    blockspergrid_C = int(math.ceil(n_C/threadsperblock[2]))
+
+    blockspergrid = (blockspergrid_H,blockspergrid_W,blockspergrid_C)
+
+    W_device = cuda.to_device(W)
+    img_device = cuda.to_device(img)
+    b_device = cuda.to_device(b)
+    Z_device = cuda.to_device(Z)
+
+    conv_step_forward4D[blockspergrid,threadsperblock](W_device,img_device,b_device,Z_device,stride,n_H,n_W,n_C)
+
+    cuda.synchronize()
+    k1 = Z_device.copy_to_host()
+    cuda.synchronize()
+
+    print(f"With GPU:{time.time()-gpu_time}")
+
+    #CPU
+    obj = Layers.ConvLayer()
+    cpu_time = time.time()
+    Z,_= obj.conv_forward(img,W,b,stride)            
+    k2 = Z.copy()
+    print(f"With CPU:{time.time()-cpu_time}")
+
+    print(np.array_equal(k1.round(6),k2.round(6)))
+    print(np.allclose(k1,k2))
+     
 
     """
     #3D
